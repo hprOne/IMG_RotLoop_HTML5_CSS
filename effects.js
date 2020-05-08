b2a5c711541b2e6f9457759ed75a1ce9b6d5a6f9
hprOne <bartek.laka@gmail.com>
2020-05-08 23:47:00 +0200

    effects.js PancakeSite v1.02

--- effects.js
+++ effects.js
@@ -1,17 +1,22 @@
-var a, b, c, img;
+var a, b, c, img, footer;
 // getting elements by id and assiging them to variables FOR BUTTONS
 a = document.getElementById("less");
 b = document.getElementById("more");
 c = document.getElementById("rotate");
 //getting element (img) by tag name
-img = document.getElementById("nalesnik");
+img = document.getElementById("pancake");
 //default rotation
 let rotation = 0;
 
+console.log(footer)
 //adding listener
 a.addEventListener("click", function () {
   let length = img.offsetWidth;
-  if (length>150)
+  if (length<=300)
+  {
+    document.getElementById("footerhprOne").style.display = "block";
+  }
+  if (length>150 )
   {
     var end = length - 10 + "px";
     img.style.width = end;
@@ -20,6 +25,10 @@ a.addEventListener("click", function () {
 
 b.addEventListener("click", function () {
   let length = img.offsetWidth;
+  if (length>=300)
+  {
+    document.getElementById("footerhprOne").style.display = "none";
+  }
   if (length<525)
   {
     var end = length + 10 + "px";
